8e884b1d635e9cb70cdbf5bbcc859c2cc30ecabe
Chris San <christianes322@gmail.com>
2023-11-07 23:47:02 -0600

    created server js

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const routes = require('./control');
+const sequelize = require('./configure/connect');
+const path = require('path');
+
+const helpers = require('./utils/data');
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
+
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'cookies',
+  cookie: {
+        expires: 10 * 60 * 1000
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
